b1ca91e1b804e7d9852cbb54c3182277292163e5
Seulgi7802 <sgi7802@kakao.com>
2022-09-15 13:33:45 +0900

    2022/09/15 김슬기 메인 애니메이션 완성

--- public/js/common.js
+++ public/js/common.js
@@ -2,4 +2,5 @@ function scroll_move(n){
     $('html, body').stop().animate({
         scrollTop : $('.section_top').eq(n).offset().top
     }, 400);
+    scroll_location($('.section_top').eq(n).offset().top, 'down')
 }
