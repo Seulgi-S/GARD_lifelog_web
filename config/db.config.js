3c0b064b3f95d1534cc4438d35836e1cb268fa38
Seulgi7802 <sgi7802@kakao.com>
2022-09-15 17:03:32 +0900

    문제 해결중1

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host : '220.67.213.212',
-    user : 'root',
-    password : 'gard',
-    databasse : 'lifelog'
-}
--- public/js/common.js
+++
@@ -1,6 +0,0 @@
-function scroll_move(n){
-    $('html, body').stop().animate({
-        scrollTop : $('.section_top').eq(n).offset().top
-    }, 400);
-    scroll_location($('.section_top').eq(n).offset().top, 'down')
-}
